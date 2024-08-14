edd2368eacaca18a0f8c66e95d2b72e0cce61688
Adrien Bolognini <duskylunari@gmail.com>
2024-08-14 09:30:18 +0200

    Update: Implemented usedStates on the app to switch pages dynamically

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 import { Home } from "./pages/Home";
@@ -8,11 +8,38 @@ import { Error } from "./pages/Error";
 
 import "./index.scss";
 
+function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    console.log('Current Page:', currentPage);
+    switch (currentPage) {
+      case 'Home':
+        return <Home />;
+      case 'Flat':
+        return <Flat />;
+      case 'About':
+        return <About />;
+      case 'Error':
+        return <Error />;
+      default:
+        return <Home />;
+    }
+  };
+
+  return (
+    <React.StrictMode>
+      <nav>
+        <button onClick={() => setCurrentPage('Home')}>Home</button>
+        <button onClick={() => setCurrentPage('Flat')}>Flat</button>
+        <button onClick={() => setCurrentPage('About')}>About</button>
+        <button onClick={() => setCurrentPage('Error')}>Error</button>
+      </nav>
+      {renderPage()}
+    </React.StrictMode>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <Home></Home> */}
-    {/* <Flat></Flat> */}
-    <About></About>
-    {/* <Error></Error> */}
-  </React.StrictMode>,
-)
+<App />
+);
