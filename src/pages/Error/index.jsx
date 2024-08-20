431c324e12776386f4b1eb39d9cdcb3ccfacd659
Adrien Bolognini <duskylunari@gmail.com>
2024-08-20 18:19:42 +0200

    Update: Implemented the Error page

--- src/pages/Error/index.jsx
+++ src/pages/Error/index.jsx
@@ -6,7 +6,11 @@ export function Error() {
     return (
         <Layout>
             <main>
-                <h1>Error</h1>
+                <div className="Error-container">
+                <h1 className="Error-container__title">404</h1>
+                <p className="Error-container__text">Oups! La page que vous demandez n'éxiste pas.</p>
+                <a className="Error-container__link" href="/">Retourner sur la page d'accueil</a>
+                </div>
             </main>
         </Layout>
     )
