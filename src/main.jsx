b74be8811266f358fc92978274c612ef6b9911cb
Adrien Bolognini <duskylunari@gmail.com>
2024-08-20 16:43:10 +0200

    Fix: Adapting main page & global scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
 
-import { Home } from "./pages/Home";
-import { Flat } from "./pages/Flat";
-import { About } from "./pages/About";
-import { Error } from "./pages/Error";
+import router from "./Router";
 
 import "./index.scss";
 
-function App() {
-  const [currentPage, setCurrentPage] = useState('Home');
-
-  const renderPage = () => {
-    console.log('Current Page:', currentPage);
-    switch (currentPage) {
-      case 'Home':
-        return <Home />;
-      case 'Flat':
-        return <Flat />;
-      case 'About':
-        return <About />;
-      case 'Error':
-        return <Error />;
-      default:
-        return <Home />;
-    }
-  };
-
-  return (
-    <React.StrictMode>
-      <nav>
-        <button onClick={() => setCurrentPage('Home')}>Home</button>
-        <button onClick={() => setCurrentPage('Flat')}>Flat</button>
-        <button onClick={() => setCurrentPage('About')}>About</button>
-        <button onClick={() => setCurrentPage('Error')}>Error</button>
-      </nav>
-      {renderPage()}
-    </React.StrictMode>
-  );
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-<App />
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
