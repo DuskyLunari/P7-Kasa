be48a903e61bd4de78c8f1c304f6fa96784171fd
Adrien Bolognini <duskylunari@gmail.com>
2024-08-20 20:16:05 +0200

    Update: Implemented About page styling and elements

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -8,13 +8,21 @@ export function About() {
     return (
         <Layout>
             <main>
-                <Header></Header>
                 <Banner></Banner>
-                <Collapse></Collapse>
-                <Collapse></Collapse>
-                <Collapse></Collapse>
-                <Collapse></Collapse>
-                <Footer></Footer>
+                <div className="Collapse-container">
+                    <Collapse title="Fiabilité">
+                        <p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
+                    </Collapse>
+                    <Collapse title="Respect">
+                        <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation de voisinage entrainera une exclusion de notre plateforme.</p>
+                    </Collapse>
+                    <Collapse title="Service">
+                        <p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation de voisinage entrainera une exclusion de notre plateforme.</p>
+                    </Collapse>
+                    <Collapse title="Sécurité">
+                        <p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des atelier sur la sécurité domestique pour nos hôtes.</p>
+                    </Collapse>
+                </div>
             </main>
         </Layout>
     )
