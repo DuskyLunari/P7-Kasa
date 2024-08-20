8043f2981aa27252e9549d890b775217c0ca73e9
Adrien Bolognini <duskylunari@gmail.com>
2024-08-20 16:57:08 +0200

    Update: Footer component implemented

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,11 @@
 import "./Footer.scss";
+import logo from "/footer-logo.png";
 
 export function Footer() {
     return (
-        <footer>
-            <p>Footer</p>
-        </footer>
-    )
-}
+      <footer className="footer">
+        <img className="footer__logo" src={logo} alt="Logo de l'agence immobilière Kasa"/>
+        <p className="footer__copyright">© 2020 Kasa. All rights reserved</p>
+      </footer>
+    );
+  }
