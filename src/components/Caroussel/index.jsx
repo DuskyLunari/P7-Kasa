d7b1a6456e96a317546a009a97feb19116bc5ec9
Adrien Bolognini <duskylunari@gmail.com>
2024-08-20 23:30:02 +0200

    Update: Implemented Caroussel component

--- src/components/Caroussel/index.jsx
+++ src/components/Caroussel/index.jsx
@@ -1,9 +1,35 @@
-import "./Caroussel.css";
+import React, { useState } from 'react';
+import './Caroussel.scss';
 
-export function Caroussel() {
+export function Caroussel({ images }) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handlePrev = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+    };
+
+    const handleNext = () => {
+        setCurrentIndex((prevIndex) =>
+            prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+    };
+
     return (
-        <main>
-            <h1>Caroussel</h1>
-        </main>
-    )
+        <div className="caroussel">
+            <button className="caroussel__arrow caroussel__arrow--left" onClick={handlePrev}>
+                <i className="fa-solid fa-chevron-left"></i>
+            </button>
+            <div className="caroussel__images">
+                <img src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+            </div>
+            <button className="caroussel__arrow caroussel__arrow--right" onClick={handleNext}>
+                <i className="fa-solid fa-chevron-right"></i>
+            </button>
+            <div className="caroussel__count">
+                {currentIndex + 1} / {images.length}
+            </div>
+        </div>
+    );
 }
