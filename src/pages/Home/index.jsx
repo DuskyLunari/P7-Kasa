df5a014eeb44880b95e9641d03ee5bdafe527988
Adrien Bolognini <duskylunari@gmail.com>
2024-08-20 15:42:12 +0200

    Update: adding datas needed from json file

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,22 +1,41 @@
-import { Header } from "../../components/Header";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Layout } from "../../components/Layout";
 import { Banner } from "../../components/Banner";
 import { Thumb } from "../../components/Thumb";
-import { Footer } from "../../components/Footer";
 
 import "./Home.scss";
 
 export function Home() {
+
+    const [logements, setLogements] = useState([]);
+
+    useEffect(() => {
+        async function fetchLogements() {
+            const res = await fetch("./logements.json");
+            const datas = await res.json();
+            setLogements(datas);
+        }
+
+        fetchLogements();
+    }, []);
+
     return (
-        <main>
-            <Header></Header>
-            <Banner></Banner>
-            <Thumb></Thumb>
-            <Thumb></Thumb>
-            <Thumb></Thumb>
-            <Thumb></Thumb>
-            <Thumb></Thumb>
-            <Thumb></Thumb>
-            <Footer></Footer>
-        </main>
-    )
+        <Layout>
+            <main>
+                <Banner />
+                <section className="thumbs">
+                    {logements.length > 0 ? (
+                        logements.map((logement) => (
+                            <Link key={logement.id} to={"/logement" + logement.id}>
+                                <Thumb title={logement.title} cover={logement.cover} />
+                            </Link>
+                        ))
+                    ) : (
+                        <p>Chargement en cours...</p>
+                    )}
+                </section>
+            </main>
+        </Layout>
+    );
 }
