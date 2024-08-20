261ce62984e32a52da8fd2bbadc37cde27dac447
Adrien Bolognini <duskylunari@gmail.com>
2024-08-21 01:09:47 +0200

    Update: Implemented Score component

--- src/components/Score/index.jsx
+++ src/components/Score/index.jsx
@@ -1,9 +1,19 @@
-import "./Score.scss";
+import React from 'react';
+import './Score.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-export function Score() {
-    return (
-        <main>
-            <h1>Score</h1>
-        </main>
-    )
+export function Score({ score }) {
+  const rating = Math.min(Math.max(parseInt(score, 10), 1), 5);
+
+  return (
+    <div className="score">
+      {[1, 2, 3, 4, 5].map((star) => (
+        <i
+          key={star}
+          className={`fa-star fa-solid ${star <= rating ? 'filled' : 'regular'}`}
+          aria-hidden="true"
+        ></i>
+      ))}
+    </div>
+  );
 }
