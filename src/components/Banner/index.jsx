642019588c5ea6df4164fcd2e6ea7b0d679e87e5
Adrien Bolognini <duskylunari@gmail.com>
2024-08-22 23:00:38 +0200

    Fix: removed main HTML element in component

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -3,18 +3,16 @@ import './Banner.scss';
 
 export function Banner({ bannerImg, text }) {
     return (
-        <main>
-            <div className="banner">
-                {text && <div className="banner__overlay"></div>}
-                <img 
-                    className="banner__img" 
-                    src={bannerImg} 
-                    alt="Banner"
-                />
-                {text && (
-                    <p className="banner__text">{text}</p>
-                )}
-            </div>
-        </main>
+        <div className="banner">
+            {text && <div className="banner__overlay"></div>}
+            <img
+                className="banner__img"
+                src={bannerImg}
+                alt="Banner"
+            />
+            {text && (
+                <p className="banner__text">{text}</p>
+            )}
+        </div>
     );
 }
