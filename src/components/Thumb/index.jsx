1e2d3a7e158b49c672730c56f87987f5bfa4f696
Adrien Bolognini <duskylunari@gmail.com>
2024-08-23 00:01:14 +0200

    Update: Component container adapted to article

--- src/components/Thumb/index.jsx
+++ src/components/Thumb/index.jsx
@@ -2,9 +2,9 @@ import "./Thumb.scss";
 
 export function Thumb({ cover, title }) {
     return (
-      <li className="thumb">
+      <article className="thumb">
         <img className="thumb__img" src={cover} alt={cover} ></img>
         <h2 className="thumb__title">{title}</h2>
-      </li>
+      </article>
     );
   }
