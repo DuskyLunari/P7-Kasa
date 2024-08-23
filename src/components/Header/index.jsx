af28b327e4892dd96a816b703eca6f81bba6c1c0
Adrien Bolognini <duskylunari@gmail.com>
2024-08-23 23:46:59 +0200

    Fix: html layout arrangement

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,30 +6,27 @@ import logo from "/logo.svg";
 export function Header() {
   return (
     <header className="header">
-        <figure className="header__logo">
-            <img src={ logo } alt="Logo de Kasa" />
-        </figure>
-        <nav className="header__nav">
-          <ul>
+      <figure className="header__logo">
+        <img src={logo} alt="Logo de Kasa" />
+      </figure>
+      <nav className="header__nav">
+        <ul>
           <li>
             <NavLink
               to="/"
-              className={({ isActive }) => (isActive ? "active" : "")}
-              
-            >
+              className={({ isActive }) => (isActive ? "active" : "")}>
               Accueil
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/a-propos"
-              className={({ isActive }) => (isActive ? "active" : "")}
-            >
+              className={({ isActive }) => (isActive ? "active" : "")}>
               A propos
             </NavLink>
           </li>
-          </ul>
-        </nav>
+        </ul>
+      </nav>
     </header>
   );
 }
